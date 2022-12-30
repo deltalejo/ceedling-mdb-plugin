4f6f972bfcf1c961395bbf55cc88fa22a7da8ac0
Alejandro Rosso <deltalejo@gmail.com>
2022-12-30 08:27:18 -0500

    Execute test fixture with serial port when hardware tool is not the simulator.

--- lib/mdb.rb
+++ lib/mdb.rb
@@ -3,25 +3,27 @@ require 'mdb_defaults'
 
 MDB_ROOT_NAME       = 'mdb'.freeze
 MDB_SYM             = MDB_ROOT_NAME.to_sym
+MDB_FIXTURE_SYM     = (MDB_ROOT_NAME + '_fixture').to_sym
 
+MDB_FIXTURE_SCRIPT  = 'mdb_fixture.rb'.freeze
+MDB_FIXTURE_PATH    = File.expand_path(File.join(File.dirname(__FILE__), '..', 'bin')).freeze
+MDB_FIXTURE         = File.join(MDB_FIXTURE_PATH, MDB_FIXTURE_SCRIPT).freeze
 MDB_CMD_FILE_SUFFIX = '_cmd.txt'.freeze
-
-def mdb_form_cmd_filepath(filepath)
-  return ceedling_form_filepath(MDB_OUTPUT_PATH, filepath, '') + MDB_CMD_FILE_SUFFIX
-end
 
 class Mdb < Plugin
   def setup
     project_config = @ceedling[:setupinator].config_hash
     mdb_defaults = {
       :tools => {
-        :mdb => DEFAULT_MDB_TOOL
+        :mdb => DEFAULT_MDB_TOOL,
+        :mdb_fixture => DEFAULT_MDB_FIXTURE_TOOL
       }
     }
     @ceedling[:configurator_builder].populate_defaults(project_config, mdb_defaults)
     
     @config = project_config[MDB_SYM]
     @tool = project_config[:tools][MDB_SYM]
+    @fixture = project_config[:tools][MDB_FIXTURE_SYM]
     
     raise unless @ceedling[:configurator_validator].exists?(@config, :device)
     raise unless @ceedling[:configurator_validator].exists?(@config, :hwtool)
@@ -37,15 +39,16 @@ class Mdb < Plugin
         :output_path => File.join(PROJECT_BUILD_ROOT, MDB_ROOT_NAME)
       },
       :tools => {
-        :mdb => @tool
+        :mdb => @tool,
+        :mdb_fixture => @fixture
       }
     }
-    mdb_config[:tools][:test_fixture] = @tool if @config[:test_fixture]
+    mdb_config[:tools][:test_fixture] = @fixture if @config[:test_fixture]
     @ceedling[:configurator].build_supplement(project_config, mdb_config)
   end
   
   def update_config(**kwargs)
-    @config.merge!(kwargs)
+    @config.deep_merge!(kwargs)
     project_config = @ceedling[:setupinator].config_hash
     @ceedling[:configurator].build_supplement(project_config, {:mdb => @config})
   end
@@ -61,6 +64,30 @@ class Mdb < Plugin
     write_command_file(test)
   end
   
+  def pre_test_fixture_execute(arg_hash)
+    return unless @config[:test_fixture]
+    
+    if @config[:hwtool] != 'sim'
+      raise 'Please specify serial port.' unless @config[:serialport][:port]
+      
+      serialport_params = {
+        :port => @config[:serialport][:port],
+        :baud => @config[:serialport][:baudrate],
+        :data_bits => @config[:serialport][:data_bits],
+        :stop_bits => @config[:serialport][:stop_bits],
+        :parity => @config[:serialport][:parity]
+      }
+      serialport_params.each do |key, val|
+        @fixture[:arguments] << "--#{key} #{val}"
+      end
+    end
+    
+    mdb_command_line = @ceedling[:tool_executor].build_command_line(
+      @tool, [], form_cmd_filepath(arg_hash[:executable])
+    )
+    @fixture[:arguments] << mdb_command_line[:line]
+  end
+  
   private
   
   def write_command_file(test)
@@ -68,7 +95,7 @@ class Mdb < Plugin
     cmd_file = form_cmd_filepath(test)
     device = @config[:device]
     hwtool = @config[:hwtool]
-    tool_properties = @config[:tools_properties].fetch(hwtool.to_sym, {})
+    tool_properties = @config[:hwtools_properties].fetch(hwtool.to_sym, {})
     
     File.open(cmd_file, 'w') do |f|
       f.puts("device #{device}")
