cbf99ba5b313a7321e214980b8f1602f283c9758
Alejandro Rosso <deltalejo@gmail.com>
2024-01-09 21:53:32 -0500

    Remove deprecated background task execution tool option.

--- lib/mdb_defaults.rb
+++ lib/mdb_defaults.rb
@@ -2,7 +2,6 @@ DEFAULT_MDB_TOOL = {
   :executable => (ENV['MDB'].nil? ? 'mdb'.ext(SystemWrapper.windows? ? '.bat' : '') : ENV['MDB'].split[0]).freeze,
   :name => 'default_mdb'.freeze,
   :stderr_redirect => StdErrRedirect::NONE.freeze,
-  :background_exec => BackgroundExec::NONE.freeze,
   :optional => false.freeze,
   :arguments => [
     '${1}'.freeze
@@ -13,7 +12,6 @@ DEFAULT_MDB_FIXTURE_TOOL = {
   :executable => (ENV['RUBY'].nil? ? FilePathUtils.os_executable_ext('ruby') : ENV['RUBY'].split[0]).freeze,
   :name => 'default_mdb_fixture'.freeze,
   :stderr_redirect => StdErrRedirect::NONE.freeze,
-  :background_exec => BackgroundExec::NONE.freeze,
   :optional => false.freeze,
   :arguments => [
     '#{MDB_FIXTURE}'.freeze
