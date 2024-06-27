383c93b2cb6c1cf0cb8ff941719d22fe2b2e33cf
Alejandro Rosso <deltalejo@gmail.com>
2024-06-27 17:11:17 -0500

    Make code intentions clearer and use Ceedling helper functions for config and tool validation.

--- lib/mdb.rb
+++ lib/mdb.rb
@@ -1,13 +1,12 @@
+require 'ceedling/exceptions'
 require 'ceedling/plugin'
 
 MDB_ROOT_NAME       = 'mdb'.freeze
 MDB_SYM             = MDB_ROOT_NAME.to_sym
-MDB_FIXTURE_SYM     = (MDB_ROOT_NAME + '_fixture').to_sym
 
 MDB_OUTPUT_PATH     = File.join(PROJECT_BUILD_ROOT, MDB_ROOT_NAME)
 MDB_FIXTURE_PATH    = File.expand_path(File.join(File.dirname(__FILE__), '..', 'bin')).freeze
 MDB_FIXTURE_SCRIPT  = 'mdb_fixture.rb'.freeze
-MDB_FIXTURE         = File.join(MDB_FIXTURE_PATH, MDB_FIXTURE_SCRIPT).freeze
 
 MDB_CMD_FILE_SUFFIX = 'cmd'.freeze
 MDB_CMD_FILE_EXT    = '.txt'.freeze
@@ -17,81 +16,89 @@ MDB_LOG_FILE_EXT    = '.xml'.freeze
 
 class Mdb < Plugin
   def setup
-    @project_config = @ceedling[:setupinator].config_hash
+    project_config = @ceedling[:setupinator].config_hash
     
-    @config = @project_config[MDB_SYM]
-    @tool = @project_config[:tools][MDB_SYM]
-    @fixture = @project_config[:tools][MDB_FIXTURE_SYM]
+    unless @ceedling[:configurator_validator].exists?(project_config, MDB_SYM)
+      walk = @ceedling[:reportinator].generate_config_walk([MDB_SYM])
+      error = "MDB plugin is enabled but missing the required configuration block `#{walk}`"
+      raise CeedlingException.new(error)
+    end
     
-    raise unless @ceedling[:configurator_validator].exists?(@config, :device)
-    raise unless @ceedling[:configurator_validator].exists?(@config, :hwtool)
+    @config = project_config[MDB_SYM]
+    
+    validate_config(@config)
     
     [:device, :hwtool].each do |key|
       if @config[key] =~ RUBY_STRING_REPLACEMENT_PATTERN
-        @config[key].replace(@ceedling[:system_wrapper].module_eval(@config[key]))
+        @config[key].replace(
+          @ceedling[:system_wrapper].module_eval(@config[key])
+        )
       end
     end
-    
-    mdb_config = {
-      :tools => {
-        :mdb => @tool,
-        :mdb_fixture => @fixture
-      }
-    }
-    mdb_config[:tools][:test_fixture] = @fixture if @config[:test_fixture]
-    @ceedling[:configurator].build_supplement(@project_config, mdb_config)
   end
   
-  def update_config(**kwargs)
-    @config.deep_merge!(kwargs)
-    @ceedling[:configurator].build_supplement(@project_config, {:mdb => @config})
+  def set_hwtool(hwtool)
+    if hwtool.nil? || hwtool.empty?
+      error = "MDB debug tool must be given"
+      raise CeedlingException.new(error)
+    end
+    
+    @config[:hwtool] = hwtool
+  end
+  
+  def set_serialport(params)
+    if params[:port].nil? || params[:port].empty?
+      error = "MDB serial port must be given"
+      raise CeedlingException.new(error)
+    end
+    
+    @config[:serialport].merge!(params)
+  end
+  
+  def pre_test(test)
+    validate_late_config()
   end
   
   def pre_test_fixture_execute(arg_hash)
-    return unless @config[:test_fixture]
+    executable = arg_hash[:executable]
     
-    Rake.application[MDB_ROOT_NAME + '_deps'].invoke
-    write_command_file(arg_hash[:executable])
+    write_command_file(executable)
     
-    @fixture_arguments = @fixture[:arguments]
-    @fixture[:arguments] = Array.new(@fixture[:arguments])
-    
-    if @config[:hwtool] != 'sim'
-      raise 'Please specify serial port.' unless @config[:serialport][:port]
-      
-      serialport_params = {
-        :port => @config[:serialport][:port],
-        :baud => @config[:serialport][:baudrate],
-        :data_bits => @config[:serialport][:data_bits],
-        :stop_bits => @config[:serialport][:stop_bits],
-        :parity => @config[:serialport][:parity]
-      }
-      serialport_params.each do |key, val|
-        @fixture[:arguments] << "--#{key} #{val}"
-      end
-    end
-    
-    mdb_command = @ceedling[:tool_executor].build_command_line(
-      @tool,
-      [
-        "--file-name #{form_log_filename(@config[:hwtool])}",
-        "--log-dir #{PROJECT_LOG_PATH}"
-      ],
-      form_cmd_filepath(arg_hash[:executable])
-    )
-    @fixture[:arguments] << '--' << mdb_command[:line]
-    
-    @ceedling[:loginator].log("MDB command: #{mdb_command}", Verbosity::DEBUG)
-    @ceedling[:loginator].log("MDB fixture: #{@fixture}", Verbosity::DEBUG)
-  end
-  
-  def post_test_fixture_execute(arg_hash)
-    return unless @config[:test_fixture]
-    
-    @fixture[:arguments] = @fixture_arguments
+    arg_hash[:tool] = TOOLS_MDB_FIXTURE
+    arg_hash[:tool][:arguments] = args_builder_fixture(executable)
   end
   
   private
+  
+  def validate_config(config)
+    unless config.is_a?(Hash)
+      walk = @ceedling[:reportinator].generate_config_walk([MDB_SYM])
+      error = "Expected configuration #{walk} to be a Hash but found #{config.class}"
+      raise CeedlingException.new(error)
+    end
+    
+    validations = []
+    validations << @ceedling[:configurator_validator].exists?(@config, :device)
+    validations << @ceedling[:configurator_validator].exists?(@config, :hwtool)
+    
+    unless validations.all?
+      error = "MDB plugin configuration failed validation"
+      raise CeedlingException.new(error)
+    end
+  end
+  
+  def validate_late_config()
+    validations = []
+    validations << @ceedling[:configurator_validator].exists?(@config, :hwtool)
+    if @config[:hwtool] != "sim"
+      validations << @ceedling[:configurator_validator].exists?(@config, :serialport, :port)
+    end
+    
+    unless validations.all?
+      error = "MDB plugin configuration failed validation"
+      raise CeedlingException.new(error)
+    end
+  end
   
   def form_cmd_filepath(filepath)
     return File.join(
@@ -104,9 +111,52 @@ class Mdb < Plugin
     return [MDB_ROOT_NAME, hwtool, MDB_LOG_FILE_SUFFIX].join("_").ext(MDB_LOG_FILE_EXT)
   end
   
-  def write_command_file(exec)
-    cmd_file = form_cmd_filepath(exec)
-    @ceedling[:loginator].log("Creating #{File.basename(cmd_file)}...", Verbosity::NORMAL)
+  def args_builder_mdb()
+    return [
+      "--log-dir #{PROJECT_LOG_PATH}",
+      "--file-name #{form_log_filename(@config[:hwtool])}"
+    ]
+  end
+  
+  def args_builder_serialport()
+    serialport_params = {
+      :port => @config[:serialport][:port],
+      :baud => @config[:serialport][:baudrate],
+      :data_bits => @config[:serialport][:data_bits],
+      :stop_bits => @config[:serialport][:stop_bits],
+      :parity => @config[:serialport][:parity]
+    }
+    args = []
+    
+    serialport_params.each do |key, val|
+      args << "--#{key} #{val}"
+    end
+    
+    return args
+  end
+  
+  def args_builder_fixture(executable)
+    mdb_fixture_filepath = File.join(MDB_FIXTURE_PATH, MDB_FIXTURE_SCRIPT)
+    args = [mdb_fixture_filepath]
+    
+    if @config[:hwtool] != 'sim'
+      args += args_builder_serialport()
+    end
+    
+    mdb_command = @ceedling[:tool_executor].build_command_line(
+      TOOLS_MDB,
+      args_builder_mdb(),
+      form_cmd_filepath(executable)
+    )
+    
+    args << '--' << mdb_command[:line]
+    
+    return args
+  end
+  
+  def write_command_file(filename)
+    cmd_file = form_cmd_filepath(filename)
+    @ceedling[:loginator].log("Creating #{File.basename(cmd_file)}...")
     
     device = @config[:device]
     hwtool = @config[:hwtool]
@@ -120,7 +170,7 @@ class Mdb < Plugin
         f.puts "set #{key} #{val}"
       end
       f.puts("hwtool #{hwtool}")
-      f.puts("program #{exec}")
+      f.puts("program #{filename}")
       breakpoints&.each do |breakpoint|
         f.puts("break #{breakpoint}")
       end
